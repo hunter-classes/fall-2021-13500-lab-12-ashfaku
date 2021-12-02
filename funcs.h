50491490d4ae845359e3a2ff7073e77a20903393
Ashfak Uddin <ashmanpapet@gmail.com>
2021-12-01 21:03:57 -0500

    Fixed small mistakes

--- funcs.h
+++ funcs.h
@@ -1,8 +1,6 @@
 #pragma once
 #include <vector>
-#include <iostream>
 using std::vector;
 vector<int> makeVector(int n);
 vector<int> goodVibes(const vector<int> &v);
-void gogeta(vector<int> &goku, vector<int> &vegeta);
-vector<int> sumPairWise(const vector<int> &v1, const vector<int> &v2)
+void gogeta(vector<int> &goku, vector<int> &vegeta); // vegito they should've meant
--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 using std::cout;
 using std::endl;
 using std::vector;
-void printVector(vector<int> &p)
+void printVector(const vector<int> &p)
 {
 	for (int i : p)
 		cout << i << " ";
@@ -19,6 +19,20 @@ void printGoodVibes(const vector<int> &p)
 {
 	cout << "The original vector: ";
 	printVector(p);
+	cout << "The goodVibes vector: ";
+	printVector(goodVibes(p));
+}
+void printFusion(vector<int> &goku, vector<int> &vegeta)
+{
+	cout << "The first fusion component is: ";
+	printVector(goku);
+	cout << "The second fusion component is: ";
+	printVector(vegeta);
+	cout << "And now they've fused, leaving first component with all elements combined and second component empty" << endl;
+	gogeta(goku, vegeta);
+	cout << "The first fusion component now is: ";
+	printVector(goku);
+	cout << "And the second fusion component's size is " << vegeta.size() << endl;
 }
 int main()
 {
@@ -28,8 +42,13 @@ int main()
 	printMakeVector(7);
 
 	cout << "Task B: goodVibes(const vector<int> &p)" << endl;
-	vector<int> t{1,2,3,-4,-5,-10,6};
-	
+	vector<int> t{1,2,3,-4,-5,-10,6}, t1{0,-9,2,3,4,-2, 4,-100,4};
+	printGoodVibes(t);
+	printGoodVibes(t1);
+	cout << "Task C: gogeta function" << endl;
+	printFusion(t, t1);
+	vector<int> s{1,2,3,4,5,6}, s1{7,8,9,10,11};
+	printFusion(s, s1);
 
 	return 0;
 
